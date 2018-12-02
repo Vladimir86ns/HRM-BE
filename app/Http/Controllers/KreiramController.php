78b190734b186568e07003a909e9d0886e33d87b
vladimir86ns <vladimir86ns@gmail.com>
2018-12-02 18:48:21 +0100

    add dingo

---
+++ app/Http/Controllers/KreiramController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class KreiramController extends Controller
+{
+    public function test()
+    {
+        return 'radi';
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$api = app(Router::class);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Http\Controllers', 'middleware' => 'api'], function (Router $api) {
+        $api->get('register-test', 'KreiramController@test');
+    });
 });
